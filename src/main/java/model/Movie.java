98c9f20df645525bf5e4404a90ee703250f28ce5
May <t0333024@my.ntu.ac.uk>
2024-10-21 01:18:02 +0100

    Movie and Review models implemented in model package

---
+++ src/main/java/model/Movie.java
@@ -0,0 +1,32 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data // take cares of all getters and setters
+@AllArgsConstructor // generates a constructor that initialize all fields of a class
+@NoArgsConstructor // generates a no-argument constructor for that class
+public class Movie {
+    @Id // to specify the unique identifier for an entity
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+
+    // define a relationship between documents->easier managing related data without embedding 1 document inside another
+    @DocumentReference
+    // embedded relationship: reviews related to this movie will be added to this list
+    private List<Review> reviewIds;
+}
