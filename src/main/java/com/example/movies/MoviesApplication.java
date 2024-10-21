edf6e51f05ad90563ab7bfbd23ac81d87b4c0b47
May <t0333024@my.ntu.ac.uk>
2024-10-21 01:37:34 +0100

    Controller class created

--- src/main/java/com/example/movies/MoviesApplication.java
+++ src/main/java/com/example/movies/MoviesApplication.java
@@ -2,10 +2,12 @@ package com.example.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"model", "controller"})
 public class MoviesApplication {
 
 	public static void main(String[] args) {
