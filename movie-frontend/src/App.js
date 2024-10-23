fcb384ac26a2410f52cb018b3e785347edb7d4ab
May <t0333024@my.ntu.ac.uk>
2024-10-23 22:35:24 +0100

    Implemented the Use State and Use Effect Hooks

--- movie-frontend/src/App.js
+++ movie-frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import {useState, useEffect} from "react";
 
 function App() {
+  // movies: store an array of movie data returned from the call to the relevant api endpoint
+  // setMovies: function that can be used to change the state of the movies variable
+  // when the state of the variable tracked by react through the useState hook is changed, the component is re-rendered by React
+  // App component will be re-rendered when the state of the movies variable changes.
+  const [movies, setMovies] = useState();
+
+  // function that will handle a HTTP get request to an endpoint that returns an array of movie data
+  const getMovies = async () => {
+    try{
+      const response = await api.get("/api/v1/movies");
+      setMovies(response.data);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  // getMovie function to be executed when the app component first loads
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
